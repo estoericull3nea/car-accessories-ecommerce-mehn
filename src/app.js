91163b899618cf857a3a6ce5d3f02e3cc1cc6ed7
Ericson Palisoc <palisocericson87@gmail.com>
2023-05-24 05:57:53 +0800

    register, login and about us added

--- src/app.js
+++ src/app.js
@@ -1,35 +1,34 @@
-const express = require('express');
-const path = require('path');
+const express = require("express");
+const path = require("path");
 const app = express();
-const hbs = require('hbs');
-require('./db/conn');
-const Register = require('./models/register');
+const hbs = require("hbs");
+require("./db/conn");
+const Register = require("./models/register");
 
 const port = process.env.PORT || 3000;
 
-const static_path = path.join(__dirname, '../public');
-const template_path = path.join(__dirname, '../templates/views');
-const partials_path = path.join(__dirname, '../templates/partials');
+const static_path = path.join(__dirname, "../public");
+const template_path = path.join(__dirname, "../templates/views");
+const partials_path = path.join(__dirname, "../templates/partials");
 
 app.use(express.json());
-app.use(express.urlencoded({extended: false}))
+app.use(express.urlencoded({ extended: false }));
 app.use(express.static(static_path));
-app.set('view engine', 'hbs');
-app.set('views', template_path);
+app.set("view engine", "hbs");
+app.set("views", template_path);
 hbs.registerPartials(partials_path);
 
+app.get("/", (req, res) => {
+  res.render("homepage_notsignin");
+});
 
-app.get('/', (req, res) => {
-    res.render('homepage');
-})
+app.get("/login", (req, res) => {
+  res.render("login");
+});
 
-app.get('/login', (req, res) => {
-    res.render('login');
-})
-
-app.get('/register', (req, res) => {
-    res.render('register');
-})
+app.get("/register", (req, res) => {
+  res.render("register");
+});
 
 // app.post('/register', async (req, res) => {
 //     try {
@@ -47,6 +46,52 @@ app.get('/register', (req, res) => {
 //     }
 // })
 
+app.post("/register", async (req, res) => {
+  try {
+    const password = req.body.password;
+    const cpassword = req.body.confirmPassword;
+
+    if (password === cpassword) {
+      const newUser = new Register({
+        email: req.body.email,
+        password: password,
+        confirmPassword: cpassword,
+      });
+
+      const registered = await newUser.save();
+      res.render("main_homepage");
+    } else {
+      res.send("password are not match");
+    }
+  } catch (err) {
+    res.status(400).send(err);
+  }
+});
+
+app.get("/main_homepage", (req, res) => {
+  res.render("main_homepage");
+});
+
+app.post("/login", async (req, res) => {
+  try {
+    const email = req.body.email;
+    const password = req.body.password;
+
+    const useremail = await Register.findOne({ email: email });
+    if (useremail.password === password) {
+      res.status(200).render("main_homepage");
+    } else {
+      res.send("password are not matching");
+    }
+  } catch (error) {
+    res.status(400).send("invalid email");
+  }
+});
+
+app.get("/about_us", (req, res) => {
+  res.render("about_us");
+});
+
 app.listen(port, () => {
-    console.log(`App is running on port: ${port}`);
-})
+  console.log(`App is running on port: ${port}`);
+});
--- src/models/register.js
+++ src/models/register.js
@@ -1,19 +1,19 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const empSchema = new mongoose.Schema({
-    username: {
-        type: String,
-        requried: true
-    },
-    password: {
-        type: String,
-        requried: true
-    },
-    confirmPassword: {
-        type: String,
-        required: true
-    }
-})
+  email: {
+    type: String,
+    requried: true,
+  },
+  password: {
+    type: String,
+    requried: true,
+  },
+  confirmPassword: {
+    type: String,
+    required: true,
+  },
+});
 
-const Register = new mongoose.model('Register', empSchema);
+const Register = new mongoose.model("Register", empSchema);
 module.exports = Register;
