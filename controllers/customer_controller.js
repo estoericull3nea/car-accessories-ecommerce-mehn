8996059ebffb7b0d0f41022c6b17462f716cdc22
Ericson Palisoc <palisocericson87@gmail.com>
2023-06-28 12:25:37 +0800

    images and js enhanced

--- controllers/customer_controller.js
+++ controllers/customer_controller.js
@@ -1,119 +1,110 @@
-const Register = require("../models/register");
-const Product = require("../models/product"); 
-const AddedList = require("../models/added_list"); 
-const UserMessage = require("../models/messageAboutUs"); 
-const AllProducts = require("../models/all_prods"); 
+const Register = require('../models/register')
+const Product = require('../models/product')
+const AddedList = require('../models/added_list')
+const UserMessage = require('../models/messageAboutUs')
+const AllProducts = require('../models/all_prods')
 
-const nodemailer = require('nodemailer');
+const nodemailer = require('nodemailer')
 
-const Token = require("../models/token"); 
-const crypto = require('crypto');
-const jwt = require('jsonwebtoken');
+const Token = require('../models/token')
+const crypto = require('crypto')
+const jwt = require('jsonwebtoken')
 
 const gTempHomepage = (req, res) => {
-    res.render("temp_homepage", { pageTitle: "Welcome to EA, Sign In First" });
+  res.render('temp_homepage', { pageTitle: 'Welcome to EA, Sign In First' })
 }
 
 const gLogin = (req, res) => {
-    res.render("login", { pageTitle: "Sign In" });
+  res.render('login', { pageTitle: 'Sign In' })
 }
 
 const gRegister = (req, res) => {
-    res.render("register", { pageTitle: "Sign Up" });
+  res.render('register', { pageTitle: 'Sign Up' })
 }
 // email service
 let transporter = nodemailer.createTransport({
-    service: 'gmail',
-    host: 'smtp.gmail.com',
-    auth: {
-        user: 'noreply050623@gmail.com',
-        pass: 'xlqhaxnjiowsxuyr'
-    }
+  service: 'gmail',
+  host: 'smtp.gmail.com',
+  auth: {
+    user: 'noreply050623@gmail.com',
+    pass: 'xlqhaxnjiowsxuyr',
+  },
 })
 
 // verify transport
 transporter.verify((err, success) => {
-    if(err) {
-        console.log(err);
-    } else {
-        console.log('Ready For Messages');
-        console.log(success);
-    }
+  if (err) {
+    console.log(err)
+  } else {
+    console.log('Ready For Messages')
+    console.log(success)
+  }
 })
 // end of email service
 
 const pRegister = async (req, res) => {
+  try {
+    const { email } = req.body
+    const password = req.body.password
+    const cpassword = req.body.confirmPassword
 
+    const existingUser = await Register.findOne({ email })
 
-    try {
-        const { email } = req.body
-        const password = req.body.password;
-        const cpassword = req.body.confirmPassword;
+    if (existingUser) {
+      res.json('Email already exist in Database')
+    } else if (password.length <= 7) {
+      res.json('Password must have at least 8 characters.')
+    } else {
+      if (password === cpassword) {
+        const newUser = new Register({
+          fullName: req.body.fullName,
+          username: req.body.username,
+          email: req.body.email,
+          password: password,
+          confirmPassword: cpassword,
+        })
 
-        const existingUser = await Register.findOne({ email })
+        await newUser.save().then((user) => {
+          // sendVerificationEmail(result, res)
 
-        if (existingUser) {
-            res.json('Email already exist in Database')
-        } else if (password.length <= 7) {
-            res.json('Password must have at least 8 characters.')
-        } else {
-            if (password === cpassword) {
-
-
-                const newUser = new Register({
-                    fullName: req.body.fullName,
-                    username: req.body.username,
-                    email: req.body.email,
-                    password: password,
-                    confirmPassword: cpassword,
-                });
-        
-                await newUser.save()
-                    .then(user => {
-                    // sendVerificationEmail(result, res)
-                   
-                    const token = new Token({ _userId: user._id, token: crypto.randomBytes(16).toString('hex')})
-                    token.save().then( (err) => {
-
-                        if(err) {
-                            console.log(err);
-                        }
-
-                        const mailOptions = {
-                            to: user.email,
-                            from: 'noreply050623@gmail.com',
-                            subject: 'Acc Verification Token',
-                            html: `Please Verify Your Account by clicking this <a href="http://${req.headers.host}/confirmation/${token.token}">link</a>`
-                        }
-
-                        transporter.sendMail(mailOptions, err => {
-                            if(err) {
-                                console.log('failed');
-                            }
-                            res.render('pleaseCheck')
-                        })
-
-                    }) // end token of save
-                }); // end of user save
-
-
-                // await newUser.save()
-                // res.redirect("/login");
-
-
-            } else {
-                res.json("password are not match");
+          const token = new Token({
+            _userId: user._id,
+            token: crypto.randomBytes(16).toString('hex'),
+          })
+          token.save().then((err) => {
+            if (err) {
+              console.log(err)
             }
-        }
 
-    } catch (err) {
-        res.status(400).send(err);
+            const mailOptions = {
+              to: user.email,
+              from: 'noreply050623@gmail.com',
+              subject: 'Acc Verification Token',
+              html: `Please Verify Your Account by clicking this <a href="http://${req.headers.host}/confirmation/${token.token}">link</a>`,
+            }
+
+            transporter.sendMail(mailOptions, (err) => {
+              if (err) {
+                console.log('failed')
+              }
+              res.render('pleaseCheck')
+            })
+          }) // end token of save
+        }) // end of user save
+
+        // await newUser.save()
+        // res.redirect("/login");
+      } else {
+        res.json('password are not match')
+      }
     }
-
+  } catch (err) {
+    res.status(400).send(err)
+  }
 }
 
 const pConfirmation = (req, res) => {
-    res.render('verified')
+  res.render('verified')
 }
 
 // const gVerify = (req, res) => {
@@ -135,190 +126,210 @@ const pConfirmation = (req, res) => {
 //       })
 // }
 
-
-
 const pLogin = async (req, res) => {
-    try {
-        const email = req.body.email;
-        const password = req.body.password;
+  try {
+    const email = req.body.email
+    const password = req.body.password
 
-        const useremail = await Register.findOne({ email: email });
-        if (useremail.password === password) {
-        // res.status(200).render("main_homepage");
-            res.redirect("/home");
-        } else {
-            res.send("Email or Password is Incorrect");
-        }
-    } catch (error) {
-        res.status(400).send("Account not found");
+    const useremail = await Register.findOne({ email: email })
+    if (useremail.password === password) {
+      // res.status(200).render("main_homepage");
+      res.redirect('/home')
+    } else {
+      res.send('Email or Password is Incorrect')
     }
+  } catch (error) {
+    res.status(400).send('Account not found')
+  }
 }
 
-const usingMiddleware = async(req, res, next) => {
-    latestUser = await Register.findOne().sort({ createdAt: -1 }).limit(1);
-    const userID = latestUser._id; 
+const usingMiddleware = async (req, res, next) => {
+  latestUser = await Register.findOne().sort({ createdAt: -1 }).limit(1)
+  const userID = latestUser._id
 
-    Register.findById(userID)
-        .then((userInDb) => {
-        req.user = userInDb;
-        next();
+  Register.findById(userID)
+    .then((userInDb) => {
+      req.user = userInDb
+      next()
     })
-    .catch((err) => console.log(err));
+    .catch((err) => console.log(err))
 }
 
 const pAddToCart = (req, res) => {
-    req.user
-        .addToCart(req.body.id)
-        .then(() => res.redirect('/home#cart-section'))
-        .catch((err) => console.log(err));
+  req.user
+    .addToCart(req.body.id)
+    .then(() => res.redirect('/home#cart-section'))
+    .catch((err) => console.log(err))
 }
 
 const gCart = (req, res) => {
-    req.user
-        .populate("cart.items.productId")
-        .then((user) => {
-            res.render("cart", {
-            cart: user.cart,
-            pageTitle: "Cart",
-    });
-    }).catch((err) => console.log(err));
+  req.user
+    .populate('cart.items.productId')
+    .then((user) => {
+      res.render('cart', {
+        cart: user.cart,
+        pageTitle: 'Cart',
+      })
+    })
+    .catch((err) => console.log(err))
 }
 
 const pDeleteItemCart = (req, res, next) => {
-    req.user
-      .removeCart(req.body.id)
-      .then(() => {
-        res.redirect("/cart");
-      })
-      .catch((err) => console.log(err));
+  req.user
+    .removeCart(req.body.id)
+    .then(() => {
+      res.redirect('/cart')
+    })
+    .catch((err) => console.log(err))
 }
 
 const gProduct = (req, res) => {
-    res.render("products", { pageTitle: "Products" });
+  res.render('products', { pageTitle: 'Products' })
 }
 
 const gOurTeam = (req, res) => {
-    res.render("our_team", { pageTitle: "Our Team" });
+  res.render('our_team', { pageTitle: 'Our Team' })
 }
 
-const pCart = async(req, res) => {
-    try {
-        const { imgURL, title, quantity, price, description } = req.body;
-        const newProd = new Product({
-          imgURL,
-          title,
-          quantity,
-          price,
-          description,
-        });
+const pCart = async (req, res) => {
+  try {
+    const { imgURL, title, quantity, price, description } = req.body
+    const newProd = new Product({
+      imgURL,
+      title,
+      quantity,
+      price,
+      description,
+    })
 
-        const ifExist = await AddedList.findOne({ imgURL: imgURL });
-        if(!ifExist) {
-            const newProd2 = new AddedList({
-                imgURL,
-                title,
-                quantity,
-                price,
-                description,
-              });
-        
-            const added = await newProd.save();
-            const added2 = await newProd2.save();
-            res.redirect('/products#prods-area')
-        } else {
-            res.redirect('/products#prods-area')
-        }
-        
-      } catch (err) {
-        res.status(400).send(err);
+    const ifExist = await AddedList.findOne({ imgURL: imgURL })
+    if (!ifExist) {
+      const newProd2 = new AddedList({
+        imgURL,
+        title,
+        quantity,
+        price,
+        description,
+      })
+
+      const added = await newProd.save()
+      const added2 = await newProd2.save()
+      res.redirect('/products#prods-area')
+    } else {
+      res.redirect('/products#prods-area')
     }
+  } catch (err) {
+    res.status(400).send(err)
+  }
 }
 
 const gFaq = (req, res) => {
-    res.render("faq", { pageTitle: "FAQ" });
+  res.render('faq', { pageTitle: 'FAQ' })
 }
 
 const gTermsnConditions = (req, res) => {
-    res.render("terms_and_conditions", { pageTitle: "Terms and Condition" });
+  res.render('terms_and_conditions', { pageTitle: 'Terms and Condition' })
 }
 
 const gPrivacyPolicy = (req, res) => {
-    res.render("privacy_policy", { pageTitle: "Privacy Policy" });
+  res.render('privacy_policy', { pageTitle: 'Privacy Policy' })
 }
 
 const gAboutUs = (req, res) => {
-    res.render("about_us", { pageTitle: "About Us" });
+  res.render('about_us', { pageTitle: 'About Us' })
 }
 
 const gHome = (req, res) => {
-    Product.find().then((products) => {
-        res.render("main_homepage", {
-            prods: products,
-            pageTitle: "Welcome to EA",
-        });
-    });
+  Product.find().then((products) => {
+    res.render('main_homepage', {
+      prods: products,
+      pageTitle: 'Welcome to EA',
+    })
+  })
 }
 
-
 const pContactUsForm = (req, res) => {
-    const { name, email, message } = req.body 
+  const { name, email, message } = req.body
 
-    let transporter = nodemailer.createTransport({
-        service: 'gmail',
-        host: 'smtp.gmail.com',
-        auth: {
-            user: 'noreply050623@gmail.com',
-            pass: 'xlqhaxnjiowsxuyr'
-        }
+  let transporter = nodemailer.createTransport({
+    service: 'gmail',
+    host: 'smtp.gmail.com',
+    auth: {
+      user: 'noreply050623@gmail.com',
+      pass: 'xlqhaxnjiowsxuyr',
+    },
+  })
+
+  const newMessage = new UserMessage({
+    name,
+    email,
+    message,
+  })
+
+  newMessage.save().then(() => {
+    const mailOptions = {
+      to: 'palisocericson87@gmail.com',
+      from: email,
+      subject: 'User Queries! || New Contact Form',
+      text: message,
+    }
+
+    transporter.sendMail(mailOptions, (err) => {
+      if (err) {
+        console.log('failed')
+      }
+      res.render('sentMessage')
     })
-
-    const newMessage = new UserMessage({
-        name, email, message
-    })
-
-     newMessage.save().then( () => {
-        const mailOptions = {
-            to: 'palisocericson87@gmail.com',
-            from: email,
-            subject: 'User Queries! || New Contact Form',
-            text: message,
-        }
-
-        transporter.sendMail(mailOptions, err => {
-            if(err) {
-                console.log('failed');
-            }
-            res.render('sentMessage')
-        })
-    })
+  })
 }
 
 const gSearchItem = (req, res) => {
-    const titleItem = req.query.searchItem
-    // Product.find().then((product) => {
-    //     res.render("searchItem", {
-    //         pageTitle: "Search Products",
-    //     });
-    // });
+  const titleItem = req.query.searchItem
+  // Product.find().then((product) => {
+  //     res.render("searchItem", {
+  //         pageTitle: "Search Products",
+  //     });
+  // });
 
-    AllProducts.find({ title: titleItem }).then(prods => {
-        console.log(prods);
-        res.render("searchItem", {
-            pageTitle: "Search Products",
-            prods,
-        });
+  AllProducts.find({ title: titleItem }).then((prods) => {
+    console.log(prods)
+    res.render('searchItem', {
+      pageTitle: 'Search Products',
+      prods,
     })
+  })
 }
 
 const gProfile = (req, res) => {
-    const user = req.user
-    res.render('profile', {pageTitle: 'Profile', user})
+  const user = req.user
+  res.render('profile', { pageTitle: 'Profile', user })
 }
 
 const pLogout = (req, res) => {
-    res.redirect('/')
+  res.redirect('/')
 }
 
 module.exports = {
-    gTempHomepage, gLogin, gRegister, pRegister, pLogin, usingMiddleware, pAddToCart, gCart, pDeleteItemCart, gProduct, gOurTeam, pCart, gFaq, gTermsnConditions,gPrivacyPolicy, gAboutUs, gHome, pConfirmation, pContactUsForm, gSearchItem, gProfile, pLogout
+  gTempHomepage,
+  gLogin,
+  gRegister,
+  pRegister,
+  pLogin,
+  usingMiddleware,
+  pAddToCart,
+  gCart,
+  pDeleteItemCart,
+  gProduct,
+  gOurTeam,
+  pCart,
+  gFaq,
+  gTermsnConditions,
+  gPrivacyPolicy,
+  gAboutUs,
+  gHome,
+  pConfirmation,
+  pContactUsForm,
+  gSearchItem,
+  gProfile,
+  pLogout,
 }
--- server.js
+++ server.js
@@ -1,55 +1,48 @@
-// if(process.env.NODE_ENV !== 'production') {
+// if (process.env.NODE_ENV !== 'production') {
 //   require('dotenv').load()
 // }
 
-const express = require("express");
-const path = require("path");
-const app = express();
+const express = require('express')
+const path = require('path')
+const app = express()
 
-const mongoose = require('mongoose');
+const mongoose = require('mongoose')
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3000
 const myRoutes = require('./routes/routes')
 
-const static_path = path.join(__dirname, "./public");
-const template_path = path.join(__dirname, "./templates/views");
-
-// const PUBLISHABLE_KEY = 'pk_test_51NI9vwDfPidN2UcqC3WsTSwYUQqlqCZQixEtyEpUivYFZvKxA195eg8IRirPPk2HVCBmCf5leYeGoVkzNACmnYM900xMGM6mGg'
-// const SECREY_KEY = 'sk_test_51NI9vwDfPidN2UcqQDKSupcUL3BqfDdz8Mo94LdFelzzNe5yL40PmM8NOmWU9Dbrm9aUNDTSuGJxJ9V9jDeF1XIS007VvlWeY2'
-
-// const stripe = require('stripe')(SECREY_KEY);
-
+const static_path = path.join(__dirname, './public')
+const template_path = path.join(__dirname, './templates/views')
 
 // setting connection
 mongoose
-  .connect("mongodb://127.0.0.1:27017/ecommerce-project", {
+  .connect('mongodb://127.0.0.1:27017/ecommerce-project', {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
   .then(() => {
-    console.log("connection success");
+    console.log('connection success')
   })
   .catch((e) => {
-    console.log(`connection failed ${e}`);
-  });
+    console.log(`connection failed ${e}`)
+  })
 
 // setting middlewares
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
 
 // setting path
-app.use(express.static(static_path));
-app.set("view engine", "ejs");
-app.set("views", template_path);
+app.use(express.static(static_path))
+app.set('view engine', 'ejs')
+app.set('views', template_path)
 
 // routes
 app.use('/', myRoutes)
 
 // app.post('/payment', async(req, res) => {
-    
+
 // })
 
 app.listen(port, () => {
-  console.log(`App is running on port: http://localhost:${port}`);
-});
+  console.log(`App is running on port: http://localhost:${port}`)
+})
