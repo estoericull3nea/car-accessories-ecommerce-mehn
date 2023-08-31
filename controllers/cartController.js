e4b4fe71215cd3000b206389e3f3ebfddcb11bb3
Ericson Palisoc <palisocericson87@gmail.com>
2023-08-31 21:56:10 +0800

    ui cart bug

--- controllers/cartController.js
+++ controllers/cartController.js
@@ -1,16 +1,23 @@
 const UserModel = require('../models/User')
 
 const getCart = async (req, res) => {
-  console.log(req.session.user)
-
   const token = req.cookies['access_token']
   if (req.session.user) {
     const user = await UserModel.findById({ _id: req.session.user._id })
     const cartCount = user.cart.items.length
 
-    res.render('cart', { pageTitle: 'Cart', token, cartCount })
+    // getting cart using populate
+    const populateProductId = await user.populate('cart.items.productId')
+    const populated = populateProductId.cart.items
+
+    res.render('cart', { pageTitle: 'Cart', token, cartCount, populated })
   } else {
-    res.render('cart', { pageTitle: 'Cart', token, cartCount: 0 })
+    res.render('cart', {
+      pageTitle: 'Cart',
+      token,
+      populated: undefined,
+      cartCount: 0,
+    })
   }
 }
 
--- models/Message.js
+++ models/Message.js
@@ -1,18 +1,21 @@
 const mongoose = require('mongoose')
 
-const MessageSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: true,
+const MessageSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+    },
+    message: {
+      type: String,
+      required: true,
+    },
   },
-  email: {
-    type: String,
-    required: true,
-  },
-  message: {
-    type: String,
-    required: true,
-  },
-})
+  { timestamps: true }
+)
 
 module.exports = new mongoose.model('Message', MessageSchema)
